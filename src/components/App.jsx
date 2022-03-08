4592c197111d1835f440b1b602b39229a3322648
Timothy <timnguy17@gmail.com>
2022-03-07 18:49:40 -0800

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import exampleVideoData from '/src/data/exampleVideoData.js';
+import VideoList from './VideoList.js';
+
 var App = () => (
   <div>
     <nav className="navbar">
@@ -10,11 +13,14 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
+        <VideoList />
         <div><h5><em>videoList</em> view goes here</h5></div>
       </div>
     </div>
   </div>
 );
+
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,24 @@
-var VideoList = () => (
+
+var VideoList = (exampleVideoData) => (
   <div className="video-list">
+    {props.videos.exampleVideoData.map( video =>
+      <VideoListEntry video={video} />
+
+    )}
+    <VideoListEntry video='exampleVideoData[0]' />
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
   </div>
+
+  //<div className="video-list">
+
+
 );
+
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
@@ -17,3 +29,19 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
+
+
+/*
+class VideoList extends React.Component {
+  coonstructor (props) {
+    super(props);
+  }
+
+  render() {
+
+    return (
+
+    )
+  }
+}
+*/
